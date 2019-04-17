f39c62c3d34ae385892a0e376d6f32f39032e327
Stefano <stefano.fontana.2000@gmail.com>
2019-04-17 13:39:09 +0200

    JWT mechanism

--- src/BACKEND/authentication.mjs
+++ src/BACKEND/authentication.mjs
@@ -1,9 +1,33 @@
 import database from "../persistence/db.connect"
+import fs from "fs"
+import jwt from "jsonwebtoken"
+import environment from "./cjs"
+
+const __dirname = environment.__dirname;
 
 const header = "auth";
 const bearer = "giovanni";
 
+const privkey = fs.readFileSync(__dirname + "/jwt.crt");
+const pubkey = fs.readFileSync(__dirname + "/jwt.key");
+
+let i  = 'tetofonta';
+let s  = 'subjecttt';
+let a  = 'loooooool';
+let signOptions = {
+    issuer:  i,
+    subject:  s,
+    audience:  a,
+    expiresIn:  "12h",
+    algorithm:  "RS256"
+};
+
 export default function auth_backend(req, res, next) {
+
+    if(req.url === "/login"){
+        next();
+        return;
+    }
 
     if (req.headers[header]) {
         if (!req.headers[header].startsWith(`${bearer} `)) {
@@ -11,10 +35,29 @@ export default function auth_backend(req, res, next) {
             return;
         }
 
-        //verifica l;autenticazione e aggiunge i dati in req.auth
+        try{
+            let token = req.headers[header].substr(bearer.length+1);
+            let result = jwt.verify(token, pubkey, signOptions);
+
+            //database("Users").findAll()
+
+            req.auth = result;
+
+        } catch (e) {
+            res.status(403).send();
+            return;
+        }
+
         next();
+        return;
     }
     res.set("WWW-Authenticate", "AUTH");
     res.status(401);
     res.end();
+}
+
+export function logincb(app){
+    app.post("/login", (req, res) => {
+        res.status(200).send(bearer + " " + jwt.sign({username: "aaa"}, privkey, signOptions));
+    });
 }
--- src/BACKEND/index.mjs
+++ src/BACKEND/index.mjs
@@ -1,13 +1,17 @@
 import express from "express"
 import {connect} from "../persistence/db.connect";
 import auth from "./authentication"
+import {logincb} from "./authentication";
 
 connect().then(() => {
     const PORT = 5000;
     const app = express();
     app.use(auth);
 
+    logincb(app);
+
     app.get("/", (req, res) => {
+        console.log(req.auth);
         res.send("hello")
     });
 
